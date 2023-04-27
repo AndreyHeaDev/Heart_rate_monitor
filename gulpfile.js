9263f822f27aebe7e53e2358cac2333020a3ff1f
AndrewHoum <sim1968sim@gmail.com>
2023-04-28 00:55:39 +0300

    1st modification for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ gulp.task('server', function() {
     browserSync({
         server: {
             baseDir: "src"
-        }
+        },
+        browser: 'chrome'   /* Запускаем в Chrome */
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload);
@@ -19,7 +20,7 @@ gulp.task('server', function() {
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
